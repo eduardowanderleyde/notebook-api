d2f8d746774b6557f18daafff8b77015c48116c6
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-07-22 17:09:17 -0300

    Render JSON with associations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,18 @@
 class Contact < ApplicationRecord
-  belongs_to :kind #, optional: true
+  belongs_to :kind 
   def author
     "Eduardo"
   end
 
+  def kind_description
+    self.kind.description
+  end
+
   def as_json(options={})
-    super(methods: :author, root: true)
+    super(
+      root: true,
+      methods: [:kind_description, :author],
+      include: { kind: { only: :description }}
+      )
   end
 end
